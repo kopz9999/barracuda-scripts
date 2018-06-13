4717a497a7f7284fe57db937cd2c22b0fae359fb
Luis Canales <kopz9999@gmail.com>
2018-06-13 11:03:04 -0500

    update checkbox selector

--- checkbox-validation.js
+++ checkbox-validation.js
@@ -1,7 +1,7 @@
 (function(jQuery) {  
   jQuery(document).ready(function() {
     MktoForms2.whenReady(function (form) {
-      var checkbox = form.getFormElem().find('input[type="checkbox"]#mktoCheckbox_17449_0');
+      var checkbox = form.getFormElem().find('.mktoCheckboxList input[type="checkbox"]');
       // var label = form.getFormElem().find('#mktoCheckbox_17449_0');
       var label = checkbox.next();
       var submit = form.getFormElem().find('button.mktoButton[type="submit"]');
