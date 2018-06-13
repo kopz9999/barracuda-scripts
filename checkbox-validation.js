d2f010620e969c700824864e1749839ec948f881
Luis Canales <kopz9999@gmail.com>
2018-06-13 10:56:08 -0500

    checkbox validation scripts done

---
+++ checkbox-validation.js
@@ -0,0 +1,22 @@
+(function(jQuery) {  
+  jQuery(document).ready(function() {
+    MktoForms2.whenReady(function (form) {
+      var checkbox = form.getFormElem().find('input[type="checkbox"]#mktoCheckbox_17449_0');
+      // var label = form.getFormElem().find('#mktoCheckbox_17449_0');
+      var label = checkbox.next();
+      var submit = form.getFormElem().find('button.mktoButton[type="submit"]');
+
+      form.submittable(false);
+
+      submit.click(function() {
+        if (checkbox.prop('checked')==true) {
+          form.submittable(true);
+          form.submit();
+        } else {
+          label.addClass('invalid');
+          label.focus();
+        }
+      });
+    });
+  });
+})(jQuery);
